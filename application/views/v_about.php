5eb7b79dfe61b6208083a9623385379dde3e8448
19220839-NabilahFadiahZahra-2023 <19220839.nabilahfz@gmail.com>
2023-10-27 13:59:38 +0700

    penambahan file baru

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section> 
+    <h1><?php echo $judul ?></h1> 
+    <img src = "<?php echo base_url()?>assets/css/pp.jpg" width="250" height="250">
+    <br> 
+    <h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Depan : Nabilah Fadiah</li> 
+        <li>Nama Belakang : Zahra</li> 
+    </ul>
+    <h4>Alamat</h4> 
+    <ul type="none"> 
+        <li> duta graha 1</li> 
+    </ul> 
+     
+    <h4>Tempat Lahir</h4> 
+    <ul type="none"> 
+        <li>Bekasi</li> 
+    </ul> 
+ 
+    <h4>Olah Raga Favorit</h4> 
+    <ul type="square"> 
+        <li>voli</li> 
+        <li>renang</li> 
+    </ul> 
+</section> 
